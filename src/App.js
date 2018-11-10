b8a11fe9d26cd7cdf19d7aff0dc10815d4617304
omcnicoll <mcnicoll.o@husky.neu.edu>
2018-11-10 11:02:16 -0500

    Adding support for ESLint and prettier

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+          <h1>Hey Hey</h1>
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
