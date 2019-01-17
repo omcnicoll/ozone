19763d5d6d7695049f80a59cdf04c1c537c8f37e
Olivier McNicoll <OMcNicoll@rightpoint.com>
2019-01-17 17:17:23 -0500

    Added palette and working on side nav

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
+import { createMuiTheme } from '@material-ui/core/styles';
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#7d7d7d',
+      main: '#515151',
+      dark: '#292929',
+      contrastText: 'ffffff'
+    },
+    secondary: {
+      light: '#ff953e',
+      main: '#ea6401',
+      dark: '#b04300',
+      contrastText: '#000000'
+    }
+  }
+});
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1>Hey Hey</h1>
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <MuiThemeProvider theme={theme}>
+          <div>
+            <NavBar />
+          </div>
+          <header className="App-header">
+            <h1>ozone</h1>
+            <p>Snippets and bikes, solve your problems here.</p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </header>
+        </MuiThemeProvider>
       </div>
     );
   }
